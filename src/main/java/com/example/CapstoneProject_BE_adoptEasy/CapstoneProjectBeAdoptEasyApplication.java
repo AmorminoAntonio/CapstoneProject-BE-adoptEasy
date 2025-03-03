05f368a0ca50b32db883f343d492a05deeac190f
Antonio Amormino <crisantoamo@gmail.com>
2025-03-03 12:23:13 +0100

    empty commit - initial c.

---
+++ src/main/java/com/example/CapstoneProject_BE_adoptEasy/CapstoneProjectBeAdoptEasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.CapstoneProject_BE_adoptEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectBeAdoptEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectBeAdoptEasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CapstoneProject_BE_adoptEasy/CapstoneProjectBeAdoptEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CapstoneProject_BE_adoptEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectBeAdoptEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
